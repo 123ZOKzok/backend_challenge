278407514ccadbf2194ba414e888a565d91e2468
123ZOKzok <zippyondiba@gmail.com>
2024-06-07 11:27:50 +0300

    Including the API URLs in the main project URL configuration.

--- backend_challenge/urls.py
+++ backend_challenge/urls.py
@@ -14,20 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-# ]
+from django.contrib import admin
 from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import CustomerViewSet, OrderViewSet
-
-router = DefaultRouter()
-router.register(r'customers', CustomerViewSet)
-router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
