05d65bdd25bd84274ba8001b26a8db5755ae0e20
123ZOKzok <zippyondiba@gmail.com>
2024-06-07 11:33:14 +0300

    adding authentication

--- api/views.py
+++ api/views.py
@@ -1,13 +1,15 @@
 from rest_framework import viewsets
 from .models import Customer, Order
 from .serializers import CustomerSerializer, OrderSerializer
-
-
+from rest_framework.permissions import IsAuthenticated
+...
 class CustomerViewSet(viewsets.ModelViewSet):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
+    permission_classes = [IsAuthenticated]
 
 class OrderViewSet(viewsets.ModelViewSet):
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
 
