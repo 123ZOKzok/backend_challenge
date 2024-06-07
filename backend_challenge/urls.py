74a4e53c030e42c41500165ea239e56a7abca017
123ZOKzok <zippyondiba@gmail.com>
2024-06-07 11:24:00 +0300

    Defining urls

--- backend_challenge/urls.py
+++ backend_challenge/urls.py
@@ -14,9 +14,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+# from django.contrib import admin
+# from django.urls import path
+
+# urlpatterns = [
+#     path('admin/', admin.site.urls),
+# ]
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import CustomerViewSet, OrderViewSet
+
+router = DefaultRouter()
+router.register(r'customers', CustomerViewSet)
+router.register(r'orders', OrderViewSet)
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
